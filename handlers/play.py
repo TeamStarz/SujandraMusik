547d6604e057d337457e526f9553d00d3aeab7e6
TeamStarz <83304626+TeamStarz@users.noreply.github.com>
2021-06-13 03:55:23 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -114,7 +114,7 @@ async def play(_, message: Message):
 
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/c364d2f8144c33bd301d5.jpg"
+        thumb_name = "https://telegra.ph/file/337d37f064475a7417159.png"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -159,7 +159,7 @@ async def play(_, message: Message):
                 )
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://telegra.ph/file/c364d2f8144c33bd301d5.jpg"
+            thumb_name = "https://telegra.ph/file/337d37f064475a7417159.png"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
